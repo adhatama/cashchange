5792d263ac1101e7cddf9cb55dc3afa8c030146c
Adhatama <am.adhatama@gmail.com>
2017-11-27 20:30:45 +0700

    add test for cash changes

--- main_test.go
+++ main_test.go
@@ -20,10 +20,12 @@ func TestCalculatePaymentChances(t *testing.T) {
 	input2 := 140000
 	testData2 := map[int][]int{
 		150000: []int{100000, 50000},
+		200000: []int{100000, 100000},
 	}
 
-	input3 := 84000
+	input3 := 83000
 	testData3 := map[int][]int{
+		84000:  []int{50000, 20000, 10000, 2000, 2000},
 		85000:  []int{50000, 20000, 10000, 5000},
 		90000:  []int{50000, 20000, 20000},
 		100000: []int{100000},
@@ -47,7 +49,50 @@ func TestCalculatePaymentChances(t *testing.T) {
 }
 
 func TestCalculateCashChanges(t *testing.T) {
+	// Given
+	moneyBills := getIdr()
 
+	input1 := 76000
+	testData1 := map[int][]int{
+		77000:  []int{1000},
+		80000:  []int{2000, 2000},
+		90000:  []int{10000, 2000, 2000},
+		100000: []int{20000, 2000, 2000},
+	}
+
+	input2 := 140000
+	testData2 := map[int][]int{
+		150000: []int{10000},
+		200000: []int{50000, 10000},
+	}
+
+	input3 := 83000
+	testData3 := map[int][]int{
+		84000:  []int{1000},
+		85000:  []int{2000},
+		90000:  []int{5000, 2000},
+		100000: []int{10000, 5000, 2000},
+	}
+
+	paymentChances1 := calculatePaymentChances(input1, moneyBills)
+	paymentChances2 := calculatePaymentChances(input2, moneyBills)
+	paymentChances3 := calculatePaymentChances(input3, moneyBills)
+
+	// When
+	cashChanges1 := calculateCashChanges(input1, paymentChances1, moneyBills)
+	cashChanges2 := calculateCashChanges(input2, paymentChances2, moneyBills)
+	cashChanges3 := calculateCashChanges(input3, paymentChances3, moneyBills)
+
+	// Then
+	if !isValidCashChanges(cashChanges1, testData1) {
+		t.Error("Not working")
+	}
+	if !isValidCashChanges(cashChanges2, testData2) {
+		t.Error("Not working")
+	}
+	if !isValidCashChanges(cashChanges3, testData3) {
+		t.Error("Not working")
+	}
 }
 
 func isValidPaymentChances(paymentChances []paymentChance, testData map[int][]int) (valid bool) {
@@ -60,7 +105,30 @@ func isValidPaymentChances(paymentChances []paymentChance, testData map[int][]in
 				break
 			}
 		}
+
+		if !valid {
+			return false
+		}
 	}
 
-	return
+	return true
+}
+
+func isValidCashChanges(cashChanges []cashChange, testData map[int][]int) (valid bool) {
+	for key, val := range testData {
+		valid = false
+
+		for _, cashChange := range cashChanges {
+			if cashChange.value == key {
+				valid = reflect.DeepEqual(cashChange.detail, val)
+				break
+			}
+		}
+
+		if !valid {
+			return false
+		}
+	}
+
+	return true
 }
